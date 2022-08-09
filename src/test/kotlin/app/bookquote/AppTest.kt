9585eea9a23b6e8693c54fc981ed594d817585e7
Daniel Demidko <demidko@farpost.com>
2022-08-10 04:56:35 +0500

    Кое-какие подвижки

--- src/test/kotlin/app/bookquote/AppTest.kt
+++ src/test/kotlin/app/bookquote/AppTest.kt
@@ -3,15 +3,14 @@ package app.bookquote
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory.getLogger
 import java.net.URL
-import java.util.zip.GZIPInputStream
 import java.util.zip.ZipInputStream
 
 internal class AppTest {
 
   @Test
   fun lol() {
-    val reader = URL("https://flibusta.is/catalog/catalog.zip").openStream().let(::ZipInputStream).bufferedReader()
+    val reader = URL("https://flibusta.is/catalog/catalog.zip").openStream().buffered()
     val log = getLogger("Lol")
-    log.warn(reader.readLine())
+    log.warn(reader.readAllBytes().size.toString())
   }
 }
