3185d66a1c32a6071771f12003132ca51b41353b
Daniel Demidko <demidko@farpost.com>
2022-08-10 04:48:10 +0500

    Кое-какие подвижки

--- src/main/kotlin/app/bookquote/App.kt
+++ src/main/kotlin/app/bookquote/App.kt
@@ -1,13 +1,12 @@
 package app.bookquote
 
 import org.apache.commons.csv.CSVFormat.newFormat
-import org.slf4j.LoggerFactory.getLogger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.scheduling.annotation.Scheduled
 import java.net.URL
+import java.util.zip.GZIPInputStream
 
 @SpringBootApplication
 class App {
@@ -17,11 +16,10 @@ class App {
 
   @Bean
   fun catalog(): List<Book> {
-    val reader = catalogUrl.openStream().bufferedReader()
+    val reader = catalogUrl.openStream().let(::GZIPInputStream).bufferedReader()
     val parser = newFormat(';').parse(reader)
     parser.use {
       return parser.map {
-        println(it)
         Book(it[0], it[1], it[2], it[3], it[4], it[5], it[6], it[7], it[8])
       }
     }
--- src/test/kotlin/app/bookquote/AppTest.kt
+++ src/test/kotlin/app/bookquote/AppTest.kt
@@ -1,16 +1,17 @@
 package app.bookquote
 
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory.getLogger
 import java.net.URL
+import java.util.zip.GZIPInputStream
+import java.util.zip.ZipInputStream
 
 internal class AppTest {
 
   @Test
   fun lol() {
-    val reader = URL("https://flibusta.is/catalog/catalog.zip").openStream().bufferedReader()
-    reader.use {
-      println(reader.readLine())
-    }
+    val reader = URL("https://flibusta.is/catalog/catalog.zip").openStream().let(::ZipInputStream).bufferedReader()
+    val log = getLogger("Lol")
+    log.warn(reader.readLine())
   }
 }
