7dd0c751178bf4750721ca4346293757ee270d9e
Daniel Demidko <demidko@farpost.com>
2023-02-03 13:56:58 +0500

    draft..

--- src/main/kotlin/app/bookquote/Catalog.kt
+++ src/main/kotlin/app/bookquote/Catalog.kt
@@ -1,14 +1,18 @@
 package app.bookquote
 
 import com.github.demidko.aot.WordformMeaning
+import org.apache.commons.csv.CSVFormat
+import org.springframework.util.MultiValueMap
 import java.net.URL
+import java.util.zip.ZipInputStream
 
 /**
  * Набор адресов с каталогом в порядке приоритета (если не получается скачать с первого, переходим к следующему, и так далее)
  */
 class Catalog(private vararg val urls: URL) {
 
-  private val lemmaToBook = mutableMapOf<WordformMeaning, MutableSet<Book>>()
+  private val books = mutableListOf<Book>()
+  private val lemmaIdToBooks = mutableMapOf<Long, MutableSet<Int>>()
 
   init {
     reloadCatalog()
@@ -17,4 +21,5 @@ class Catalog(private vararg val urls: URL) {
   fun reloadCatalog() {
 
   }
+  
 }
--- src/test/kotlin/app/bookquote/AppTest.kt
+++ src/test/kotlin/app/bookquote/AppTest.kt
@@ -1,8 +1,9 @@
 package app.bookquote
 
+import org.apache.commons.csv.CSVFormat.newFormat
+import org.apache.commons.csv.CSVRecord
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory.getLogger
-import java.io.ByteArrayInputStream
 import java.net.URL
 import java.util.zip.ZipInputStream
 
@@ -10,15 +11,34 @@ internal class AppTest {
 
   @Test
   fun lol() {
+
+    val log = getLogger("lol")
+    log.warn(javaClass.getResource("/catalog.zip").toString())
     val reader =
       URL("https://flibusta.is/catalog/catalog.zip")
-        .readBytes()
-    val log = getLogger("Lol")
+        .openStream()
+        .let(::ZipInputStream)
+        .apply(ZipInputStream::getNextEntry)
+        .bufferedReader()
+        .let(newFormat(';')::parse)
+    val books = reader.use {
+      reader.drop(1).map(::parseBook)
+    }
+    log.warn("{}", books.size)
+  }
 
-    log.warn(reader.size.toString())
-
-    val lines = reader.let(::ByteArrayInputStream).let(::ZipInputStream)
-
-    log.warn(lines.readAllBytes().size.toString())
+  fun parseBook(r: CSVRecord): Book {
+    val it = r.iterator()
+    return Book(
+      it.next(),
+      it.next(),
+      it.next(),
+      it.next(),
+      it.next(),
+      it.next(),
+      it.next(),
+      it.next(),
+      it.next()
+    )
   }
 }
